12ccf38d3e7acadaaefde2e4309ce6e788f7c991
WildBabaloo <47115136+WildBabaloo@users.noreply.github.com>
2024-01-12 02:26:30 -0500

    Init branche

--- lib/main.dart
+++ lib/main.dart
@@ -130,5 +130,9 @@ class PageVisualierSondages extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     throw UnimplementedError();
+    // Plan to to make a nav bar at the bottom with an add button to make a new sondage along side a deconnexion button 
+    // On top of the navbar will just be a list of sondages that will be taken from a list create in app state with already predefined values
+    // I might need to make a class for sondage as well as answers in the sondage 
+    // But for now i can just focus on sondage since someone else will be doing the answers 
   }
 }
