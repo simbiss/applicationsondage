8fd7623e32faf9ad58a618012f98fd957642b733
WildBabaloo <47115136+WildBabaloo@users.noreply.github.com>
2024-01-12 02:54:37 -0500

    Ajout l'importation de ChangeNotifier et creation de MyAppState

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Application Sondage',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Application Sondage'),
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Application Sondage',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
+        ),
+        home: const MyHomePage(title: 'Application Sondage'),
+      ),  
     );
   }
+}
+
+class MyAppState extends ChangeNotifier {
+  var sondages = <String>[];
 }
 
 class MyHomePage extends StatefulWidget {
@@ -128,6 +120,7 @@ class _MyHomePageState extends State<MyHomePage> {
 class PageVisualierSondages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
     // TODO: implement build
     throw UnimplementedError();
   }
